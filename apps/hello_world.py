7fccdd3dda396f52f9e61a8e9c782c939f045383
quirin.niedernhuber <quirin.niedernhuber@gmail.com>
2025-08-24 19:25:47 +0000

    initialize hello world

---
+++ apps/hello_world.py
@@ -0,0 +1,13 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+# Define a class for your app, inheriting from Hass.
+class HelloWorld(hass.Hass):
+
+    # The initialize() method is the entry point for the app.
+    def initialize(self):
+        """
+        This method is called when the app is initialized.
+        """
+        # Use self.log() to print messages to the AppDaemon log.
+        self.log("Hello from your first AppDaemon app!")
+        self.log("If you see this, your git-based workflow is correctly configured.")
